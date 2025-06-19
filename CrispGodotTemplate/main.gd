7ae617882cfeaa22b3e146c18df79c4ff4e71460
Manny-OnGit <manny.fluss@gmail.com>
2025-06-18 19:56:33 -0700

    menu

--- CrispGodotTemplate/main.gd
+++ CrispGodotTemplate/main.gd
@@ -1,2 +1,28 @@
 @tool
 extends Control
+
+
+var _game_scene : PackedScene = preload("res://CrispGodotTemplate/main.tscn")
+
+@onready var _title: RichTextLabel = %Title
+@onready var _game_description: RichTextLabel = %GameDescription
+
+
+
+@export
+var titleText : String = "":
+	set(value):
+		titleText = value
+		if _title:  # Check if _title is initialized
+			_title.text = "[center]" + value
+			
+
+@export_multiline
+var descriptionText : String = "":
+	set(value):
+		descriptionText = value
+		if _game_description:  # Check if _title is initialized
+			_game_description.text = value
+
+func _ready() -> void:
+	pass
